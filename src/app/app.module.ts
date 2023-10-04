bbabffbce6d4eef666e9758f54f03a92bab6d1f3
Reaien <ivanduarteherrera@gmail.com>
2023-10-04 12:54:41 -0300

    ejemploAPIsemedescargaelnote

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ServicerestService } from './services/servicerest.service';
+
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },ServicerestService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
